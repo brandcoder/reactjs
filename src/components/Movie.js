6eb721c7a92f772ec8a82945f9eb16a14516f0fb
Luke.K <34545494+gooddeng78@users.noreply.github.com>
2022-02-06 23:48:17 +0900

    css적용완료

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,24 @@
 
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({id, title, img, summary, genres}){ // Movie(props)
+function Movie({id, title, img, summary, year, genres}){ // Movie(props)
     return(
-        <div >
-            <img src={img} alt={title} />
-            <h2><Link to={`/movie/${id}`}>{title}</Link></h2> 
-            {/* <h2><a href="/movie">{title}</a></h2> */}
-            <p>{summary}</p>
-            <ul>
-            {genres.map((item) => (
-                <li key={item}>{item}</li>
-            ))}
-            </ul>
+        <div className={styles.movie}>
+            <img src={img} alt={title} className={styles.movie__img}/>
+            <div>
+                <h2 className={styles.movie__title}>
+                <Link to={`/movie/${id}`}>{title}</Link>
+                </h2>
+                <h3 className={styles.movie__year}>{year}</h3>
+                <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+                <ul className={styles.movie__genres}>
+                {genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+                </ul>
+            </div>
         </div>
 
     )
